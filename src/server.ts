e31b4887a1ba52bb9de910d7968b6c628885e4d0
Jonathan Bell <jon@jonbell.net>
2024-08-06 11:47:29 -0400

    .

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import Express from "express";
+import CORS from 'cors';
+
+// create the server, call it app
+const app = Express();
+app.use(CORS());
+
+const inputPort = process.env.PORT || 4001;
+
+app.listen(inputPort);
+type ParamType = { numPorts: string };
+let curPort = 10000;
+app.get("/:numPorts", (req: Express.Request<ParamType>, res) => {
+  const numPorts = Number.parseInt(req.params.numPorts);
+  console.log(numPorts);
+  curPort += numPorts;
+  res.status(200).send(`${curPort}`);
+});
+
+
+
+
